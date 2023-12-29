a3d3878b40a9b9af19c87df86dbdbe6d204f9f9b
ADMIN <ADMIN@PC310>
2023-12-29 09:25:15 +0700

    Add project files.

---
+++ BT01/ProgramBT01.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT01
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+        public Rectangle()
+        {
+            length = 1.0;
+            width = 1.0;
+        }
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public void setLength(double length)
+        {
+            this.length = length;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+        public void setWidth(double width)
+        {
+            this.width = width;
+        }
+        public double getWidth()
+        {
+            return width;
+        }
+        public double findArea()
+        {
+            return length * width;
+        }
+        public double findPerimeter()
+        {
+            return (length + width) * 2;
+        }
+    }
+    class ProgramBT01
+    {
+        static void Main(string[] args)
+        {       
+                do
+                {
+                    Console.OutputEncoding = Encoding.UTF8;
+                    Rectangle r1;
+                    Rectangle r2;
+                    r1 = new Rectangle();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("**********************************");
+                    Console.Write(" -Nhập chiều dài: ");
+                    double length = double.Parse(Console.ReadLine());
+                    Console.Write(" -Nhập chiều rộng: ");
+                    double width = double.Parse(Console.ReadLine());
+                    Console.WriteLine("**********************************");
+                    r2 = new Rectangle(length, width);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("**********************************");
+                    Console.WriteLine(" -Thông tin của hình chữ nhật r1:");
+                    Console.WriteLine(" +Chiều dài: {0}\n +Chiều rộng: {1}\n +Diện tích: {2}\n +Chu vi: {3}",
+                        r1.getLength(), r1.getWidth(), r1.findArea(), r1.findPerimeter());
+                    Console.WriteLine("**********************************");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("**********************************");
+                    Console.WriteLine(" -Thông tin của hình chữ nhật r2:");
+                    Console.WriteLine(" +Chiều dài: {0}\n +Chiều rộng: {1}\n +Diện tích: {2}\n +Chu vi: {3}",
+                        r2.getLength(), r2.getWidth(), r2.findArea(), r2.findPerimeter());
+                    Console.WriteLine("**********************************");
+                    Console.ReadLine();
+                    Console.WriteLine("Nhấn phím bất kì để tiếp tục. ESC để thoát !");
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+    }
+}
---
+++ BT02/ProgramBT02.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT02
+{
+    class ProgramBT02
+    {
+        class Account
+        {
+            private string id;
+            private string name;
+            private int balance;
+            public Account(string id, string name, int balance)
+            {
+                this.id = id;
+                this.name = name;
+                this.balance = balance;
+            }
+            public string getID()
+            {
+                return id;
+            }
+            public string getName()
+            {
+                return name;
+            }
+            public int getBalance()
+            {
+                return balance;
+            }
+            public void Credit(int amount)
+            {
+                if (amount > 0)
+                    balance += amount;
+            }
+            public void Debit(int amount)
+            {
+                if (amount <= balance)
+                    balance -= amount;
+                else
+                    Console.WriteLine("Số tiền vượt quá mức qui định!");
+            }
+            public void TranferTo(Account acc, int amount)
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                    acc.balance += amount;
+                }
+                else
+                    Console.WriteLine("Số tiền vượt quá mức qui định!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("Nhập id của bạn: ");
+            string id = Console.ReadLine();
+            Console.Write("Nhập tên của bạn: ");
+            string name = Console.ReadLine();
+            Console.Write("Nhập số tiền trong tài khoản của bạn: ");
+            int balance = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("---------------------------------------------");
+            Account my = new Account(id, name, balance);
+            Console.Write("Nhập id bạn của bạn: ");
+            string id1 = Console.ReadLine();
+            Console.Write("Nhập tên bạn của bạn: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Nhập số tiền trong tài khoản bạn của bạn: ");
+            int balance1 = int.Parse(Console.ReadLine());
+            Account myFriend = new Account(id1, name1, balance1);
+            Console.WriteLine("---------------------------------------------");
+            //Thông tin ban đầu
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("Thông tin ban đầu của 2 tài khoản");
+            Console.WriteLine("MyAccount: [ id:{0}, name:{1}, balance:{2} ] ",
+                my.getID(), my.getName(), my.getBalance());
+            Console.WriteLine("MyFriendAccount: [ id:{0}, name:{1}, balance:{2} ] ",
+             myFriend.getID(), myFriend.getName(), myFriend.getBalance());
+            Console.WriteLine("***********************************************************");
+            //Thực hiện giao dịch
+            int amount = 0;
+            //Rút tiền
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("***********************************************************");
+            Console.Write("Nhập số tiền bạn muốn rút: ");
+            amount = int.Parse(Console.ReadLine());
+            my.Debit(amount);
+            //Chuyển khoản
+            Console.Write("Nhập số tiền bạn muốn chuyển khoản: ");
+            amount = int.Parse(Console.ReadLine());
+            my.TranferTo(myFriend, amount);
+            Console.WriteLine("***********************************************************");
+            //Sau khi giao dịch
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("Thông tin sau khi thực hiện giao dịch của 2 tài khoản");
+            Console.WriteLine("MyAccount: [ id:{0}, name:{1}, balance:{2} ] ",
+                my.getID(), my.getName(), my.getBalance());
+            Console.WriteLine("MyFriendAccount: [ id:{0}, name:{1}, balance:{2} ] ",
+             myFriend.getID(), myFriend.getName(), myFriend.getBalance());
+            Console.WriteLine("***********************************************************");
+            Console.ReadLine();
+        }
+    }
+}
